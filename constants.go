322f6b4b40c1a95ab220b56c6c71080d0008e827
Man J Gajjar <131265476+ManJGajjar@users.noreply.github.com>
2025-12-15 12:06:24 +0530

    Add files via upload

---
+++ constants.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+const (
+	L = 5
+	B = 10
+	Area = L * B
+)
+
+func PrintConstantsExample() {
+	fmt.Println("Given Length :", L)
+	fmt.Println("Given Breadth :", B)
+	fmt.Println("Area Calculated By Above Values :", Area)
+}
