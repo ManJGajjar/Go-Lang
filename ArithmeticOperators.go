1280b3a49e05d34e7f7571d5d6200272d2a67a2b
Man J Gajjar <131265476+ManJGajjar@users.noreply.github.com>
2025-12-16 23:00:17 +0530

    Add files via upload

---
+++ ArithmeticOperators.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func ArithmeticOperators() {
+	// Example of arithmetic operators
+	fmt.Println("List of Arithmetic Operators in GoLang")
+	fmt.Println("Addition: 5 + 3 =", 5+3)
+	fmt.Println("Subtraction: 5 - 3 =", 5-3)
+	fmt.Println("Multiplication: 5 * 3 =", 5*3)
+	fmt.Println("Division: 6 / 3 =", 6/3)
+	fmt.Println("Modulus: 5 % 3 =", 5%3)
+}
