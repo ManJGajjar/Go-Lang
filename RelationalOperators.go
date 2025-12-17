4cf22cb249f40c0564d5246dbba4481aaa5e0189
Man J Gajjar <131265476+ManJGajjar@users.noreply.github.com>
2025-12-17 16:54:30 +0530

    Add files via upload

---
+++ RelationalOperators.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func RelationalOperators() {
+	// Example of relational operators
+	fmt.Println("List of Relational Operators in GoLang")		
+	fmt.Println("Equal to: 10 == 5 =", 10 == 5)
+	fmt.Println("Not equal to: 10 != 5 =", 10 != 5)
+	fmt.Println("Greater than: 10 > 5 =", 10 > 5)
+	fmt.Println("Less than: 10 < 5 =", 10 < 5)
+	fmt.Println("Greater than or equal to: 10 >= 5 =", 10 >= 5)
+	fmt.Println("Less than or equal to: 10 <= 5 =", 10 <= 5)
+}
