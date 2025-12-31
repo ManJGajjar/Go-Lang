82d627ddd9fb7ef74ed28782c6657b7df3bd6f17
Man J Gajjar <131265476+ManJGajjar@users.noreply.github.com>
2025-12-31 16:24:36 +0530

    Add files via upload

---
+++ Maps.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func MapsExample() {
+    // Creating and initializing a map with names and ages
+    ages := map[string]int{
+        "A": 30,
+        "B": 25,
+        "C": 35,
+    }
+
+    // Retrieving and printing B's age
+    fmt.Println("B's age:", ages["B"])
+}
