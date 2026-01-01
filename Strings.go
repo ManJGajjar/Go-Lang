e707c6cba60c44e26af8565f08743c8137921f69
Man J Gajjar <131265476+ManJGajjar@users.noreply.github.com>
2026-01-01 23:35:32 +0530

    Add files via upload

---
+++ Strings.go
@@ -0,0 +1,18 @@
+package main 
+
+import "fmt"
+ 
+func StringsExample() {
+ 
+    // Creating and initializing a
+    // variable with a string
+    // Using shorthand declaration
+    My_value_1 := "Lewis Hamilton"
+	
+    var My_value_2 string
+    My_value_2 = "Max Verstappen"
+ 
+    // Displaying strings
+    fmt.Println("String 1: ", My_value_1)
+    fmt.Println("String 2: ", My_value_2)
+}
