4b96baead659164eae436489f584bb1abdac8ca7
Man J Gajjar <131265476+ManJGajjar@users.noreply.github.com>
2026-01-03 23:10:28 +0530

    Add files via upload

---
+++ Rune.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+    "reflect"
+)
+
+func RuneExample() {
+
+    rune1 := 'B'
+    rune2 := 'g'
+    rune3 := '\a'
+
+    fmt.Printf("Rune 1: %c; Unicode: %U; Type: %s", rune1, rune1, reflect.TypeOf(rune1))
+    
+    fmt.Printf("\nRune 2: %c; Unicode: %U; Type: %s", rune2, rune2, reflect.TypeOf(rune2))
+    
+    fmt.Printf("\nRune 3: Unicode: %U; Type: %s", rune3, reflect.TypeOf(rune3))
+
+}
