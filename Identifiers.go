5821f4de6dda03af1e6bdf4d0ecf0acfad394e09
Man J Gajjar <131265476+ManJGajjar@users.noreply.github.com>
2026-01-06 22:40:32 +0530

    Add files via upload

---
+++ Identifiers.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+var myVariable int
+const pi = 3.14
+
+func myFunction() {}
+
+type person struct {
+   name string
+   age int
+}
+
+func myfunc() {
+   fmt.Println("Hello, World!")
+}
