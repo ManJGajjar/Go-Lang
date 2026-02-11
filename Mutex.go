7a206380b46a363005e82b3db766b624e3fc71dc
Man J Gajjar <131265476+ManJGajjar@users.noreply.github.com>
2026-02-11 06:31:56 +0530

    Add files via upload

---
+++ Mutex.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var (
+	counter int
+	mutex   sync.Mutex
+)
+
+func increment(wg *sync.WaitGroup) {
+	defer wg.Done()
+	mutex.Lock()
+	counter++
+	mutex.Unlock()
+}
+
+func MutexEx() {
+	var wg sync.WaitGroup
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go increment(&wg)
+	}
+
+	wg.Wait()
+	fmt.Printf("Final Counter Value: %d\n", counter)
+}
