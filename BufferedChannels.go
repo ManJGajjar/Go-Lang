2329b539683b5b96a928a1e415caf2a9bf1eba86
Man J Gajjar <131265476+ManJGajjar@users.noreply.github.com>
2026-02-14 11:23:16 +0530

    Remove unnecessary comments and clean up code

--- BufferedChannels.go
+++ BufferedChannels.go
@@ -6,25 +6,22 @@ import (
 )
 
 func BChannelsEx() {
-	// Create a buffered channel with capacity 2
 	ch := make(chan int, 2)
 
-	// Sender goroutine
 	go func() {
 		fmt.Println("Sending message 1")
 		ch <- 1
 		fmt.Println("Sending message 2")
 		ch <- 2
 		fmt.Println("Attempting to send message 3 (will wait)")
-		ch <- 3 // Blocks because buffer is full
+		ch <- 3 
 		fmt.Println("Message 3 sent")
 	}()
 
-	// Simulate receiver delay
 	time.Sleep(3 * time.Second)
 
-	// Receiver
 	fmt.Println("Receiving message:", <-ch)
 	fmt.Println("Receiving message:", <-ch)
 	fmt.Println("Receiving message:", <-ch)
+
 }
