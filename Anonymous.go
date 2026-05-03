454ced4a6c9d983076984e2008f775b03af29b00
Man J Gajjar <131265476+ManJGajjar@users.noreply.github.com>
2026-05-03 23:26:10 +0530

    change

--- Anonymous.go
+++ Anonymous.go
@@ -2,7 +2,7 @@ package main
 import "fmt"
 func AnonymousExample() {
    greet := func() {
-      fmt.Println("Hello, Peter Griffin.")
+      fmt.Println("Hello, Stewie Griffin.")
    }
    greet() 
 
